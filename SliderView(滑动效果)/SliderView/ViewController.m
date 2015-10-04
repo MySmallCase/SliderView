42200954ea7be519ebb46f3756c19155ae79825a
xiangyunfei <978771447@qq.com>
2015-10-04 18:42:25 +0800

    create project
    create project

---
+++ SliderView(滑动效果)/SliderView/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  SliderView
+//
+//  Created by MyMac on 15/10/4.
+//  Copyright © 2015年 MyMac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"滑动效果");
+    
+}
+
+
+
+@end
